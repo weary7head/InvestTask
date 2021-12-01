4047a2454e5f49ba03ae012171e9b041572d5278
weary7head <53371891+weary7head@users.noreply.github.com>
2021-12-01 22:31:33 +0200

    Initial commit

---
+++ InvestTask/Investment.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace InvestTask
+{
+    class Investment
+    {
+        private DateTime _agreementDate;
+        private Decimal _dollars;
+        private int _interestRate;
+        private int _investmentDuration;
+        private int _numberOfPayments => CalculateNumberOfPayments();
+        decimal _ratePerMonth => CalculateRatePerMonth();
+
+        public Investment(DateTime agreementDate, Decimal x, int r, int n)
+        {
+            _agreementDate = agreementDate;
+            _dollars = x;
+            _interestRate = r;
+            _investmentDuration = n;
+        }
+
+        public string[] GetPayments(DateTime calculationDate)
+        {
+            decimal paymentsSum = 0;
+            decimal paymentAmount = CalculatePaymentAmount();
+            int monthCount = CalculateMonthCount(calculationDate);
+            string[] payments = new string[monthCount];
+            for (int i = 0; i < monthCount; i++)
+            {
+                if (i == monthCount - 1)
+                {
+                    payments[i] = $"{i + 1}. Payment Amount: {_dollars - paymentsSum}";
+                }
+                else
+                {
+                    payments[i] = $"{i + 1}. Payment Amount: {paymentAmount}";
+                    paymentsSum += paymentAmount;
+                }
+            }
+
+            return payments;
+        }
+
+        private decimal CalculatePaymentAmount()
+        {
+            decimal paymentAmount = 0;
+
+            decimal numerator = _dollars * _ratePerMonth * Convert.ToDecimal(Math.Pow((1 + ((double)_ratePerMonth)), _numberOfPayments));
+            decimal denominator = Convert.ToDecimal(Math.Pow((1 + ((double)_ratePerMonth)), _numberOfPayments) - 1);
+
+            paymentAmount = (numerator / denominator);
+
+            return Math.Round(paymentAmount, 2);
+        }
+
+        private void CalculatePrincipalAmount()
+        {
+
+        }
+
+        private void CalculateInterestAmount()
+        {
+
+        }
+
+        private int CalculateNumberOfPayments()
+        {
+            return (_investmentDuration * 12);
+        }
+
+        private decimal CalculateRatePerMonth()
+        {
+            return ((_interestRate / 100m) / 12m);
+        }
+
+        private int CalculateMonthCount(DateTime calculationDate)
+        {
+            int months = 0;
+            DateTime temporaryDate = _agreementDate;
+
+            while (true)
+            {
+                temporaryDate = temporaryDate.AddMonths(1);
+                months++;
+                if (temporaryDate.Month == calculationDate.Month && temporaryDate.Year == calculationDate.Year)
+                {
+                    break;
+                }
+            }
+
+            return months;
+        }
+    }
+}
---
+++ InvestTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InvestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime agreementDate = DateTime.Now;
+            
+            Decimal x = 1200;
+            int r = 12;
+            int n = 1;
+
+            Console.WriteLine($"Agreement date: {agreementDate}");
+           
+            Console.WriteLine($"X: {x}");
+            Console.WriteLine($"R: {r}");
+            Console.WriteLine($"N: {n}");
+
+            Investment investment = new Investment(agreementDate, x, r, n);
+
+            string[] strings = investment.GetPayments(agreementDate.AddYears(1));
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                Console.WriteLine(strings[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
